ade518f9a3b4f7bd680c315f94465e4eaa017e27
junsu <cpzzansu@gmail.com>
2023-11-07 00:32:55 +0900

    11월 6일 수정사항 수정완료

--- src/frontend/src/components/user/views/Directions.vue
+++ src/frontend/src/components/user/views/Directions.vue
@@ -17,7 +17,7 @@
         style="margin-bottom: 45px"
       >
         <img
-          src="/images/footer-logo.png"
+          src="/images/mini-logo.png"
           style="width: 60px; height: 60px; margin-right: 25px"
         />
         <div class="col row justify-between q-col-grow">
